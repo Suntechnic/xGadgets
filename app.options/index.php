570cb0a19ca452a50b597143baba9bc186c5d9a2
Александр Маджугин <madzhugin@gmail.com>
2024-11-18 00:59:24 +0300

    добавление поддержки hint'ов, примеров и вкладок

--- app.options/index.php
+++ app.options/index.php
@@ -2,8 +2,14 @@
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 if ($arParams['PERMISSION'] <= "R") die();
 
+$arGadgetParams['RND_STRING'] = randString(8);
 
-$refOptions = \App\Settings::getOptionsInfo();
+$refOptions = array_filter(\App\Settings::getOptionsInfo(), function ($dctO) {
+        return ($dctO['title'] && $dctO['type']);
+    });
+// отфильтруем только опции с описанным типом и тайтлом
+
+
 if (
         $_SERVER['REQUEST_METHOD'] == 'POST' 
         && $_REQUEST['agm-gadget'] == 'Y' 
@@ -14,100 +20,104 @@ if (
             \App\Settings::setOption($CodeOption,$Value);
         }
     }
-
+    LocalRedirect('/bitrix/admin/index.php');
 }
+
+
+
+
+$lstTabs = [];
+$refTabs = ['more' => ['index' => 0, 'options' => []]];
+foreach ($refOptions as $CodeOption=>$dctOption) {
+    if (!$dctOption['tab']) $dctOption['tab'] = 'Разное';
+    $TabCode = md5($dctOption['tab']);
+    if (!$refTabs[$TabCode]) {
+        $refTabs[$TabCode] = ['index' => count($lstTabs), 'options' => []];
+        $lstTabs[] = [
+                'DIV' => $TabCode,
+                'TAB' => $dctOption['tab']
+            ];
+    }
+
+    $refTabs[$TabCode]['options'][] =  $CodeOption;
+}
+
 
 ?>
 <form action="" method="post">
     <input type="hidden" name="agm-gadget" value="Y">
     <input type="hidden" name="agm-gadget-id" value="<?=$id?>">
     <?=bitrix_sessid_post()?>
-    <h3>Настройки</h3>
-    <table>
-        <?foreach ($refOptions as $CodeOption=>$dctOption): if (!$dctOption['title'] || !$dctOption['type']) continue;?>
-        <tr>
-            <td class="adm-detail-content-cell-l"><?=$dctOption['title'];?></td>
-            <td class="adm-detail-content-cell-r">
-                <?if($dctOption['type'] == 'bool'):?>
-                <input 
-                        type="hidden" 
-                        name="x_app_options[<?=$CodeOption?>]" 
-                        value="0"
-                    />
-                <input 
-                        type="checkbox" 
-                        <?if (\App\Settings::getOption($CodeOption)):?>checked<?endif;?> 
-                        name="x_app_options[<?=$CodeOption?>]" 
-                        value="1"
-                    />
-                <?elseif($dctOption['type'] == 'string'):?>
-                <input 
-                        type="text" 
-                        <?if (\App\Settings::getOption($CodeOption)):?>checked<?endif;?> 
-                        name="x_app_options[<?=$CodeOption?>]" 
-                        value="<?=str_replace('"','',\App\Settings::getOption($CodeOption))?>"
-                    />
-                <?elseif($dctOption['type'] == 'integer'):?>
-                <input 
-                        type="num" 
-                        <?if (\App\Settings::getOption($CodeOption)):?>checked<?endif;?> 
-                        name="x_app_options[<?=$CodeOption?>]" 
-                        value="<?=\App\Settings::getOption($CodeOption)?>"
-                    />
-                <?endif?>
-            </td>
-        </tr>
-        <?endforeach?>
-        <?/*
-        <tr>
-            <td class="adm-detail-content-cell-l">Выключить онлайн кредитование для раздела Уценка</td>
-            <td class="adm-detail-content-cell-r"><input type="checkbox" <? if ($arResult['ONLINE_CREDIT_UC'] == 1):?>checked<? endif; ?> name="ONLINE_CREDIT_UC" value="1" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Скрыть цены на сайте</td>
-            <td class="adm-detail-content-cell-r"><input type="checkbox" <? if ($arResult['HIDE_PRICES'] == 1):?>checked<? endif; ?> name="HIDE_PRICES" value="1" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Фраза для вывода вместо цены</td>
-            <td class="adm-detail-content-cell-r"><input type="text" name="PRICE_PHRASE" value="<?=$arResult['PRICE_PHRASE'];?>" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Отключить оформление заказа</td>
-            <td class="adm-detail-content-cell-r"><input type="checkbox" <? if ($arResult['ORDER_OFF'] == 1):?>checked<? endif; ?> name="ORDER_OFF" value="1" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Процент увеличения Второй цены</td>
-            <td class="adm-detail-content-cell-r"><input type="number" name="SPRICE_PERCENT" value="<?=$arResult['SPRICE_PERCENT'];?>" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Ограничить количество покупки 1 товара</td>
-            <td class="adm-detail-content-cell-r"><input type="checkbox" <? if ($arResult['LIMIT_QUANTITY'] == 1):?>checked<? endif; ?> name="LIMIT_QUANTITY" value="1" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Значение ограничения количества покупки одного товара</td>
-            <td class="adm-detail-content-cell-r"><input type="number" name="LIMIT_VALUE" value="<?=$arResult['LIMIT_VALUE'];?>" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Включить предупреждение об изменении характеристик производителем</td>
-            <td class="adm-detail-content-cell-r"><input type="checkbox" <? if ($arResult['CHARACTERISTICS_ON'] == 1):?>checked<? endif; ?> name="CHARACTERISTICS_ON" value="1" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Фраза об изменении характеристик производителем</td>
-            <td class="adm-detail-content-cell-r"><input type="text" name="CHARACTERISTICS_PHRASE" value="<?=$arResult['CHARACTERISTICS_PHRASE'];?>" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Текст о цене в карточке товара</td>
-            <td class="adm-detail-content-cell-r"><textarea cols="20" rows="5" type="text" name="PRICE_TEXT_PRODUCT"><?=$arResult['PRICE_TEXT_PRODUCT'];?></textarea></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Адрес доступа DaData</td>
-            <td class="adm-detail-content-cell-r"><input type="text" name="DADATA_URL" value="<?=$arResult['DADATA_URL'];?>" /></td>
-        </tr>
-        <tr>
-            <td class="adm-detail-content-cell-l">Токен доступа DaData</td>
-            <td class="adm-detail-content-cell-r"><input type="text" name="DADATA_TOKEN" value="<?=$arResult['DADATA_TOKEN'];?>" /></td>
-        </tr>
-        */?>
-    </table>
+
+    <?
+    if (count($lstTabs) > 1) {
+        $tabControl = new \CAdminViewTabControl("tabControl", $lstTabs);
+        $tabControl->Begin();
+    }
+
+    foreach ($lstTabs as $dctTab) {
+        $lstOptionsCode = $refTabs[$dctTab['DIV']]['options'];
+        if ($tabControl) $tabControl->BeginNextTab();
+        ?>
+        <table>
+            <?foreach ($lstOptionsCode as $CodeOption): $dctOption = $refOptions[$CodeOption]?>
+            <tr>
+                <td class="adm-detail-content-cell-l">
+                    <?if ($dctOption['hint']):?>
+                    <span id="app_options_<?=$arGadgetParams['RND_STRING']?>_<?=$CodeOption?>">?</span>
+                    <script>
+                    BX.hint_replace(
+                            BX('app_options_<?=$arGadgetParams['RND_STRING']?>_<?=$CodeOption?>'), 
+                            '<?=CUtil::JSEscape($dctOption['hint']);?>'
+                        );
+                    </script>
+                    <?endif?>
+                    <?=$dctOption['title'];?>
+                </td>
+                <td class="adm-detail-content-cell-r">
+                    <?if($dctOption['type'] == 'bool'):?>
+                    <input 
+                            type="hidden" 
+                            name="x_app_options[<?=$CodeOption?>]" 
+                            value="0"
+                        />
+                    <input 
+                            type="checkbox" 
+                            <?if (\App\Settings::getOption($CodeOption)):?>checked<?endif;?> 
+                            name="x_app_options[<?=$CodeOption?>]" 
+                            value="1"
+                        />
+                    <?elseif($dctOption['type'] == 'string'):?>
+                    <input 
+                            type="text"
+                            name="x_app_options[<?=$CodeOption?>]" 
+                            value="<?=str_replace('"','',\App\Settings::getOption($CodeOption))?>"
+                        />
+                    <?elseif($dctOption['type'] == 'text'):?>
+                    <textarea
+                            style="width:100%"
+                            name="x_app_options[<?=$CodeOption?>]" 
+                        ><?=\App\Settings::getOption($CodeOption)?></textarea>
+                    <?elseif($dctOption['type'] == 'integer'):?>
+                    <input 
+                            type="num"
+                            name="x_app_options[<?=$CodeOption?>]" 
+                            value="<?=\App\Settings::getOption($CodeOption)?>"
+                        />
+                    <?endif?>
+                </td>
+                
+            </tr>
+            <?if ($dctOption['exampleMethod']):?>
+            <tr><td colspan="2">
+                <?=$dctOption['exampleMethod']()?>
+            </td></tr>
+            <?endif?>
+            <?endforeach?>
+        </table>
+        <?
+    }
+    if ($tabControl) $tabControl->End();
+    ?>
     <input type="submit" value="Сохранить" class="adm-btn-save" />
 </form>
